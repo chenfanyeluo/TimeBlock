f7e2c2d7491fa067ca6cc770496be738ea68c127
foreverqyq <56214794@qq.com>
2026-06-06 16:25:16 +0800

    。

--- vite.config.js
+++ vite.config.js
@@ -5,18 +5,18 @@ import path from 'path'
 export default defineConfig({
   plugins: [vue()],
   base: './',
-  root: path.join(__dirname, 'renderer'),
+  root: path.join(__dirname, 'src', 'renderer'),
   build: {
     outDir: path.join(__dirname, 'dist'),
     emptyOutDir: true
   },
   resolve: {
     alias: {
-      '@': path.join(__dirname, 'renderer'),
-      '@components': path.join(__dirname, 'renderer/components'),
-      '@views': path.join(__dirname, 'renderer/views'),
-      '@stores': path.join(__dirname, 'renderer/stores'),
-      '@utils': path.join(__dirname, 'renderer/utils')
+      '@': path.join(__dirname, 'src', 'renderer'),
+      '@components': path.join(__dirname, 'src', 'renderer', 'components'),
+      '@views': path.join(__dirname, 'src', 'renderer', 'views'),
+      '@stores': path.join(__dirname, 'src', 'renderer', 'stores'),
+      '@utils': path.join(__dirname, 'src', 'renderer', 'utils')
     }
   }
 })
