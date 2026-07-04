4e5bdee7873f5998a48d32d4122173b843c04471
foreverqyq <56214794@qq.com>
2026-07-04 10:56:11 +0800

    revert: 回退 init.sql，移除 reset_token / reset_token_expires_at 字段

--- database/init.sql
+++ database/init.sql
@@ -33,8 +33,6 @@ CREATE TABLE `users` (
   `password`    VARCHAR(255)  NOT NULL                COMMENT '加密密码 (bcrypt)',
   `name`        VARCHAR(100)  NOT NULL                COMMENT '用户姓名',
   `avatar`      VARCHAR(500)  NULL     DEFAULT NULL   COMMENT '头像URL',
-  `reset_token`      VARCHAR(255)  NULL     DEFAULT NULL   COMMENT '密码重置令牌（哈希存储）',
-  `reset_token_expires_at` DATETIME NULL  DEFAULT NULL   COMMENT '密码重置令牌过期时间',
   `created_at`  DATETIME      NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `updated_at`  DATETIME      NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
   `deleted_at`  DATETIME      NULL     DEFAULT NULL   COMMENT '删除时间 (NULL=未删除)',
